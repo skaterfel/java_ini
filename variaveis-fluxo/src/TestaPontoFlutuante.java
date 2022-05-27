a791efd5b54ea8fc1ced08160ffa1fdb7ad96d3f
Felipe Melo <felipe.leandro2@gmail.com>
2022-05-27 20:10:44 -0300

    variaveis java

---
+++ variaveis-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,8 @@
+
+public class TestaPontoFlutuante {
+	public static void main(String[] args) {
+		double salario = 2033;
+		
+		System.out.println(salario);
+	}
+}
---
+++ variaveis-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,7 @@
+
+public class TestaVariaveis {
+	public static void main(String[] args) {
+		int idade = 2021-1989;
+		System.out.println(idade);
+	}
+}
