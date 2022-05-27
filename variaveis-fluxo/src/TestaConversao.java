3bd05a8389c2338c3520921245fe4c200178f241
Felipe Melo <felipe.leandro2@gmail.com>
2022-05-27 20:21:49 -0300

    variaveis java tipos

---
+++ variaveis-fluxo/src/TestaConversao.java
@@ -0,0 +1,26 @@
+
+public class TestaConversao {
+	public static void main(String[] args) {
+		double salario = 1270.50;
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+		// casting
+		
+		long numeroGrande = 2457634874L;
+		
+		short valorPequeno = 1244;
+		
+		byte ehByte = 127;
+		
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		double total = valor1+valor2;
+		System.out.println(total);
+		//IEEE
+		
+		float pontoFlutuante = 3.14f;
+		
+		
+	}
+}
