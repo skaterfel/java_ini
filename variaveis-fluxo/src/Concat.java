f54e2d694fd73b1847b29fcb15c452f38108fd84
Felipe Melo <felipe.leandro2@gmail.com>
2022-05-28 10:51:08 -0300

    Trabalhando com Characteres

---
+++ variaveis-fluxo/src/Concat.java
@@ -0,0 +1,11 @@
+
+public class Concat {
+	public static void main(String[] args) {
+		String saudacao = "Olá, meu nome é ";
+		String nome = "Felipe";
+		String continuacao = " e minha idade é ";
+		int idade = 32;
+		
+		System.out.println(saudacao+nome+continuacao+idade+" anos.");
+	}
+}
