854380af171d8b1dafbbb9ff1e8b3edd0cb1ed94
Felipe Melo <felipe.leandro2@gmail.com>
2022-05-28 21:59:06 -0300

    correcao acento

--- condicionais/src/TestaCondicoes.java
+++ condicionais/src/TestaCondicoes.java
@@ -1,14 +1,14 @@
 public class TestaCondicoes {
 	public static void main(String[] args) {
 
-		System.out.println("Testando condições");
+		System.out.println("Testando condiï¿½ï¿½es");
 		int idade = 10;
 		int quantidadePessoas = 0;
 		if (idade >= 18) {
 			System.out.println("Manor de idade!");
 		} else {
 			if (quantidadePessoas >= 2) {
-				System.out.println("Voce é menor e esta acompanhado");
+				System.out.println("Voce Ã© menor e esta acompanhado");
 			} else {
 				System.out.println("Menor de Idade!");
 
