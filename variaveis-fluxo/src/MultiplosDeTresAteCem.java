c1f87a2ca2f795c68f4329960db2a4257e77bfbe
Felipe Melo <felipe.leandro2@gmail.com>
2022-05-30 22:57:42 -0300

    testando % modulo

--- variaveis-fluxo/src/MultiplosDeTresAteCem.java
+++ variaveis-fluxo/src/MultiplosDeTresAteCem.java
@@ -1,4 +1,10 @@
 
 public class MultiplosDeTresAteCem {
-
+	public static void main(String[] args) {
+		for(int indice = 1;indice <= 100 ;indice++ ) {
+			if(indice % 7 == 0) {
+				System.out.println(indice);
+			}
+		}
+	}
 }
