a3ebec5fd6bfbe6ed9d23be92b94bc709fdc1c6d
Felipe Melo <felipe.leandro2@gmail.com>
2022-05-30 22:49:43 -0300

    testando for 2

---
+++ variaveis-fluxo/src/TestesLacos2.java
@@ -0,0 +1,12 @@
+public class TestesLacos2 {
+	public static void main(String[] args) {
+		for(int linha = 1; linha <= 10; linha++) {
+			for(int coluna = 1; coluna <= 10; coluna++) {
+				if(coluna > linha)
+					break;
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
