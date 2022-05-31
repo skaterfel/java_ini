c675fb91e60d4744d5640bb7e08777bc460b7886
Felipe Melo <felipe.leandro2@gmail.com>
2022-05-30 21:27:21 -0300

    Testando SwitchCase

---
+++ condicionais/src/TesteSemana.java
@@ -0,0 +1,34 @@
+
+public class TesteSemana {
+	public static void main(String[] args) {
+		String diaSemana = "Sexta".toLowerCase();
+		int dia = 0;
+		switch (diaSemana) {
+		case "segunda":
+			dia = 2;
+			break;
+		case "terça":
+			dia = 3;
+			break;
+		case "quarta":
+			dia = 4;
+			break;
+		case "quinta":
+			dia = 5;
+			break;
+		case "sexta":
+			dia = 6;
+			break;
+		case "sabado":
+			dia = 7;
+			break;
+		case "domingo":
+			dia = 1;
+			break;
+
+		default:
+			break;
+		}
+		System.out.println("Hoje é " + diaSemana + "! O " + dia + "º dia da semana");
+	}
+}
