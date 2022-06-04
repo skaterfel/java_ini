d24418a37cbb3f0bc1605057583f76602c753743
Felipe Melo <felipe.leandro2@gmail.com>
2022-06-04 20:31:10 -0300

    encapsulamento - janvando

--- bytebank-encapsulado/src/TestaGetESet.java
+++ bytebank-encapsulado/src/TestaGetESet.java
@@ -2,8 +2,7 @@
 public class TestaGetESet {
 
 	public static void main(String[] args) {
-		Conta conta = new Conta();
-		conta.setNumero(1337);
+		Conta conta = new Conta(1337, 10082);
 		System.out.println(conta.getNumero());
 		
 		Cliente felipe = new Cliente();
