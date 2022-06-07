f6a61536221dc44fe05a811febcbcd6e8c91b8f8
Felipe Melo <felipe.leandro2@gmail.com>
2022-06-06 22:54:01 -0300

    Polymorfismo! Conta Corrente e Poupança

---
+++ bytebank-herdado-conta/src/Conta.java
@@ -0,0 +1,87 @@
+
+public class Conta {
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	private static int total;
+	
+	
+	
+	
+	public Conta(int agencia,int numero) {	
+		Conta.total++;
+		// System.out.println("O total de contas abertas no ByTeBank é de: " + total);
+		this.agencia = agencia;
+		this.numero = numero;
+		
+		
+	}
+
+	public void deposita(double valor) {
+		this.saldo += valor;
+	}
+
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean transfere(double valor, Conta destino) {
+		if (this.saldo >= valor) {
+			saca(valor);
+			destino.deposita(valor);
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public double getSaldo() {
+		return this.saldo;
+
+	}
+
+	public int getNumero() {
+		return this.numero;
+	}
+
+	public void setNumero(int numero) {
+		if (numero <= 0) {
+			System.out.println("Valor da conta melhor que 1!");
+			return;
+		}
+		this.numero = numero;
+	}
+
+	public int getAgencia() {
+		return this.agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		
+		if (agencia <= 0) {
+			System.out.println("Valor da conta melhor que 1!");
+			return;
+		}
+		this.agencia = agencia;
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+
+	public Cliente getTitular() {
+		return titular;
+	}
+	
+	public static int getTotal() {
+		return Conta.total;
+	}
+
+}
